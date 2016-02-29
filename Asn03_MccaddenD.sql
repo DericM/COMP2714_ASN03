bdafd1cf741dc2635dc13ced13576531723078cb
DericM <dmccadden@my.bcit.ca>
2016-02-28 22:43:33 -0800

    c

--- Asn03_MccaddenD.sql
+++ Asn03_MccaddenD.sql
@@ -10,10 +10,11 @@ SPOOL C:\Users\Deric\workspace\COMP2714_ASN03\Asn03_MccaddenD.txt
 --  email: dmccadden@my.bcit.ca
 --
 -- ---------------------------------------------------------
---  START C:\Users\Deric\workspace\COMP2714_ASN03\Asn03_MccaddenD.sql
---
---  build database
+--  BUILD DATABASE
 --  START C:\Users\Deric\workspace\COMP2714_ASN03\Asn3SetupHotels.sql
+--
+--  ASSIGNMENT
+--  START C:\Users\Deric\workspace\COMP2714_ASN03\Asn03_MccaddenD.sql
 -- ---------------------------------------------------------
 --
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
@@ -27,26 +28,30 @@ FROM DUAL;
 --using join?
 SELECT AVG(r.price) Average
 FROM Room r 
-INNER JOIN Hotel h
-  ON r.hotelNo = h.hotelNo
+  INNER JOIN Hotel h ON h.hotelNo = r.hotelNo
+WHERE h.hotelAddress LIKE'%London%'
 ; 
 --A)------
 --using IN?
-SELECT AVG(r.price) Average
-FROM Room r, Hotel h
-WHERE r.hotelNo IN h.hotelNo
+SELECT AVG(price) Average
+FROM Room
+WHERE hotelNo IN (
+  SELECT hotelNo
+  FROM Hotel
+  WHERE hotelAddress LIKE'%London%'
+)
 ;
 --
 --Q2--------------------------------------------------------
 --How many different guests have made bookings in 2016-02? 
 --for each hotel, hotel name order.
-SELECT DISTINCT h.hotelName, 
-       COUNT(g.guestName) Distinct_Guests
+SELECT h.hotelName, COUNT(DISTINCT b.guestNo) Distinct_Guests
 FROM Booking b
-NATURAL JOIN Guest g
-NATURAL JOIN Hotel h
-WHERE b.dateFrom >= DATE '2016-02-01'
+  JOIN Hotel h ON h.hotelNo = b.hotelNo
+WHERE (dateTo IS NULL OR dateTo >= DATE'2016-02-01') 
+  AND dateFrom < DATE'2016-03-01'
 GROUP BY h.hotelName
+ORDER BY h.hotelName
 ;
 --
 --Q3--------------------------------------------------------
@@ -54,26 +59,24 @@ GROUP BY h.hotelName
 --including the name of the guest staying in the room.
 --List in hotelNo, roomNo roder
 SELECT h.hotelName, 
-       h.hotelNo,
        r.roomNo, 
        r.type, 
        r.price, 
        g.guestname AS Booked_By
 FROM Room r
-INNER JOIN (
-  SELECT *
-  FROM Hotel
-  WHERE hotelName LIKE '%Grosvenor%'
-) h 
-    ON r.hotelNo = h.hotelNo
-LEFT JOIN (
-  SELECT * 
-  FROM Booking 
-  WHERE DATE '2016-02-01' BETWEEN dateFrom AND DateTo
-) b 
+  INNER JOIN (
+    SELECT *
+    FROM Hotel
+    WHERE hotelName LIKE '%Grosvenor%'
+  ) h ON r.hotelNo = h.hotelNo
+  LEFT JOIN (
+    SELECT * 
+    FROM Booking 
+    WHERE dateFrom <= DATE'2016-02-01' 
+      AND (dateTo IS NULL OR dateTo >= DATE'2016-02-01')
+  ) b 
     ON h.hotelNo = b.hotelNo AND r.roomNo = b.roomNo
-LEFT JOIN Guest g 
-    ON b.guestNo = g.guestNo
+  LEFT JOIN Guest g ON b.guestNo = g.guestNo
 ORDER BY r.hotelNo, r.roomNo
 ; 
 --
@@ -82,90 +85,56 @@ ORDER BY r.hotelNo, r.roomNo
 --List in hotelNo, roomNo oder
 --use NOT IN
 SELECT *
-FROM (
-  SELECT h.hotelNo, 
-         h.hotelName, 
-         r.roomNo, 
-         r.type, 
-         r.price, 
-         b.guestNo
-  FROM Room r
-  INNER JOIN (
-    SELECT *
-    FROM Hotel
-    WHERE hotelName LIKE '%Grosvenor%'
-  ) h 
-      ON r.hotelNo = h.hotelNo
-  LEFT JOIN (
-    SELECT *
-    FROM Booking 
-    WHERE DATE '2016-02-01' BETWEEN dateFrom AND DateTo
-  ) b 
-    ON h.hotelNo = b.hotelNo 
-    AND r.roomNo = b.roomNo
+FROM Room r
+WHERE roomNo NOT IN (
+  SELECT roomNo
+  FROM Booking
+  WHERE dateFrom <= DATE'2016-02-01' 
+    AND (dateTo IS NULL OR dateTo >= DATE'2016-02-01')
 )
-WHERE guestNo NOT IN (
-  SELECT guestNo
-  FROM Guest
-  WHERE guestNo IS NOT NULL
- )
-OR guestNo IS NULL
-ORDER BY hotelNo, roomNo
-;
+AND hotelNo NOT IN (
+  SELECT HotelNo
+  FROM Hotel
+  WHERE hotelName NOT LIKE'%Grosvenor%'
+)
+ORDER BY r.hotelNo, r.roomNo
+;  
 --
 --Q5--------------------------------------------------------
 --not exists
 SELECT *
-FROM (
-  SELECT h.hotelNo, h.hotelName, r.roomNo, r.type, r.price, b.guestNo
-  FROM Room r
-  INNER JOIN (
-    SELECT *
-    FROM Hotel
-    WHERE hotelName LIKE '%Grosvenor%'
-  ) h 
-    ON r.hotelNo = h.hotelNo
-  LEFT JOIN (
-    SELECT *
-    FROM Booking 
-    WHERE DATE '2016-02-01' BETWEEN dateFrom AND DateTo
-  ) b 
-    ON h.hotelNo = b.hotelNo 
-    AND r.roomNo = b.roomNo
-) hb
+FROM Room r
 WHERE NOT EXISTS (
   SELECT *
-  FROM Guest g
-  WHERE g.guestNo = hb.guestNo
- )
-ORDER BY hotelNo, roomNo
-;   
+    FROM Booking b
+    WHERE b.roomNo = r.roomNo
+     AND dateFrom <= DATE'2016-02-01' 
+     AND (dateTo IS NULL OR dateTo >= DATE'2016-02-01'))
+AND NOT EXISTS (
+  SELECT *
+    FROM Hotel h
+    WHERE h.hotelNo = r.hotelNo
+      AND h.hotelName NOT LIKE'%Grosvenor%')
+ORDER BY r.hotelNo, r.roomNo
+;
 --
 --Q6--------------------------------------------------------
 --List the rooms that are currently unocupied at all 'Grosvenor' hotels
 --List in hotelNo, roomNo roder
 --use LEFT JOIN
-SELECT h.hotelName, 
-       h.hotelNo,
-       r.roomNo, 
-       r.type, 
-       r.price
+SELECT h.hotelName, r.roomNo, r.type, r.price
 FROM Room r
-INNER JOIN (
-  SELECT *
-  FROM Hotel
-  WHERE hotelName LIKE '%Grosvenor%'
-  ) h 
-    ON r.hotelNo = h.hotelNo
-LEFT JOIN (
-  SELECT * 
-  FROM Booking 
-  WHERE DATE '2016-02-01' BETWEEN dateFrom AND DateTo
-  ) b 
-    ON h.hotelNo = b.hotelNo 
-    AND r.roomNo = b.roomNo
-LEFT JOIN Guest g ON b.guestNo = g.guestNo
+  LEFT JOIN Hotel h ON h.hotelNo = r.hotelNo
+  LEFT JOIN (
+    SELECT *
+    FROM Booking
+    WHERE dateFrom <= DATE'2016-02-01' 
+    AND (dateTo IS NULL OR dateTo >= DATE'2016-02-01')
+  ) b ON b.hotelNo = h.hotelNo 
+                      AND b.roomNo = r.roomNo
+  LEFT JOIN Guest g ON g.guestNo = b.guestNo
 WHERE g.guestNo is NULL
+AND h.hotelName LIKE '%Grosvenor%'
 ORDER BY r.hotelNo, r.roomNo
 ; 
 --
@@ -173,28 +142,17 @@ ORDER BY r.hotelNo, r.roomNo
 --List the rooms that are currently unocupied at all 'Grosvenor' hotels
 --List in hotelNo, roomNo roder
 --use MINUS
-SELECT h.hotelNo, 
-       r.roomNo, 
-       b.guestNo
-FROM Room r
-INNER JOIN (
-  SELECT *
-  FROM Hotel
-  WHERE hotelName LIKE '%Grosvenor%'
-) h 
-  ON r.hotelNo = h.hotelNo
-LEFT JOIN (
-  SELECT *
-  FROM Booking 
-  WHERE DATE '2016-02-01' BETWEEN dateFrom AND DateTo
-) b 
-  ON h.hotelNo = b.hotelNo 
-  AND r.roomNo = b.roomNo
-MINUS 
-  SELECT *
-  FROM Guest g,
-  WHERE b.guestNo = g.guestNo
-ORDER BY h.hotelNo, r.roomNo
+SELECT r.hotelNo, r.roomNo, r.type, r.price
+FROM Room r, Booking b
+WHERE r.hotelNo = b.hotelNo 
+AND b.dateFrom <= DATE'2016-02-01' 
+AND (b.dateTo IS NULL OR b.dateTo >= DATE'2016-02-01')
+  MINUS (
+    SELECT r.hotelNo, r.roomNo, r.type, r.price
+    FROM Room r, Hotel h
+    WHERE r.hotelNo = h.hotelNo 
+    AND h.hotelName NOT LIKE '%Grosvenor%'
+  )
 ;
 --
 --Q8--------------------------------------------------------
@@ -203,7 +161,7 @@ SELECT AVG(numBookings) AS Average
 FROM (
   SELECT COUNT(b.guestNo) AS numBookings
   FROM Hotel h
-  LEFT JOIN Booking b
+  JOIN Booking b
     ON h.hotelNo = b.hotelNo
   GROUP BY h.hotelNo
 )
@@ -219,13 +177,13 @@ SELECT r.hotelNo,
 FROM Room r
 INNER JOIN Hotel h
   ON r.hotelNo = h.hotelNo
-INNER JOIN (
+LEFT JOIN (
   SELECT *
   FROM Booking
     WHERE dateFrom <= DATE'2016-02-01'
     AND (dateTo IS NULL OR dateTo >= DATE'2016-02-01')
-) b
-  ON r.hotelNo = b.hotelNo
+) b ON r.hotelNo = b.hotelNo
+    AND b.roomNo = r.roomNo
 GROUP BY r.hotelNo, h.hotelName
 ORDER BY hotelNo
 ;
@@ -234,24 +192,27 @@ ORDER BY hotelNo
 --Create a view containing the account for each guest at grosenvor hotel
 --Use 2016-02-01 as date, and guestAccount as view name
 --
-CREATE VIEW guestAccount 
-AS
+CREATE VIEW guestAccount AS
 SELECT r.roomNo AS Room, 
        g.guestName AS Name, 
        dateFrom AS CheckIn, 
        dateTo AS CheckOut
-FROM Guest g
-LEFT JOIN Booking b
-  ON g.guestNo = b.guestNo
+FROM Guest g=
 INNER JOIN (
   SELECT *
-  FROM Hotel
-  WHERE hotelName LIKE '%Grosvenor Hotel%'
-) h
-  ON b.hotelNo = h.hotelNo
-LEFT JOIN Room r
-  ON h.hotelNo = r.hotelNo
+  FROM Booking b
+  INNER JOIN (
+    SELECT hotelName
+    FROM Hotel
+    WHERE hotelName LIKE '%Grosvenor Hotel%'
+  )  ON hotelNo = b.hotelNo
+) bh ON bh.hotelNo = g.hotelNo
+LEFT JOIN Room r ON h.hotelNo = r.hotelNo
 ;
+SELECT *
+FROM guestAccount
+;
+DROP VIEW guestAccount;
 --
 --END--------------------------------------------------------
 --
